827bb735aec18468e4dcb67b05800fe0330b17e4
Maksim Shiryaev <shimady563@gmail.com>
2024-09-27 22:16:46 +0300

    refactor: refactor some code

--- java/src/main/java/hakaton/webcommit/webCommit/Services/EmailService.java
+++ java/src/main/java/hakaton/webcommit/webCommit/Services/EmailService.java
@@ -5,7 +5,6 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import jakarta.mail.util.StreamProvider;
 
 @Service
 public class EmailService {
