f8fa379f0e31b4fbf7c08a941ebd2a8d8e1821a7
Maksim Shiryaev <shimady563@gmail.com>
2024-09-28 14:57:41 +0300

    feat: add swagger documentation

--- java/src/main/java/hakaton/webcommit/webCommit/controller/UserController.java
+++ java/src/main/java/hakaton/webcommit/webCommit/controller/UserController.java
@@ -3,6 +3,10 @@ package hakaton.webcommit.webCommit.controller;
 import hakaton.webcommit.webCommit.entity.dto.UserInfo;
 import hakaton.webcommit.webCommit.entity.dto.UserRequest;
 import hakaton.webcommit.webCommit.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,7 +30,37 @@ public class UserController {
     }
 
     @GetMapping("")
+    @SecurityRequirement(name = "SessionAuth")
     public UserInfo getUser() {
         return userService.getCurrentUserInfo();
+    }
+
+    // for documenting logout endpoint
+    @PostMapping("/logout")
+    @Operation(summary = "Logout current user")
+    @SecurityRequirement(name = "SessionAuth")
+    public void logout() {
+    }
+
+    // for documenting login endpoint
+    // I use request params to , but I couldn't find a way to represent it in Swagger
+    @PostMapping(value = "/login")
+    @Operation(
+            summary = "Login user",
+            description = "Spring uses form-data for login by default, " +
+                    "but I couldn't find a way to represent it in Swagger, " +
+                    "you can find the right request format in the postman documentation")
+    public void login(
+            @RequestParam("username")
+            @Parameter(description = "User email. " +
+                    "I use email instead of username, because it's unique in my db schema",
+                    example = "email@mail.com"
+            )
+            String email,
+
+            @RequestParam("password")
+            @Parameter(description = "User password", example = "StrongPassword123$")
+            String password
+    ) {
     }
 }
